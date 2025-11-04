d48ab737d3657f11177502fad0523efcbe1d0f6b
Jxst-Dxni <d.saracoglu@hgs-singen.online>
2025-11-04 10:10:10 +0000

    server erweiterung

---
+++ Objekorientierter-Entwurf08.01/04_Webanwendung/client.js
@@ -0,0 +1,24 @@
+document.getElementById("spielerForm").addEventListener("submit", async function(event) {
+    event.preventDefault(); // Seite soll nicht neu geladen werden
+
+    // Formulardaten sammeln
+    const spieler = {
+        name: document.getElementById("name").value,
+        jahrgang: parseInt(document.getElementById("jahrgang").value),
+        staerke: parseFloat(document.getElementById("staerke").value),
+        torschuss: parseFloat(document.getElementById("staerke").value)
+    };
+
+    // Daten an den Server senden
+    const response = await fetch("https://literate-happiness-pjp75w97g5j926wxq-12345.app.github.dev/spieler", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(spieler)
+    });
+
+    // Antwort anzeigen
+    const ergebnis = await response.json();
+    document.getElementById("serverAntwort").textContent = JSON.stringify(ergebnis, null, 2);
+});
